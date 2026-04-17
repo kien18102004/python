ab11177d895da2b971336be021966d88e43151d9
Kiên nè <kienne@MacBook-Pro-cua-Kien.local>
2026-04-17 09:39:42 +0700

    nop bai 1

---
+++ bai1.py
@@ -0,0 +1,15 @@
+
+dai = float(input("Nhập chiều dài đáy hình chữ nhật (cm): "))
+rong = float(input("Nhập chiều rộng đáy hình chữ nhật (cm): "))
+cao = float(input("Nhập chiều cao hình khối chữ nhật (cm): "))
+
+
+so_le = int(input("Số lượng số lẻ cần hiển thị: "))
+
+
+dien_tich = dai * rong
+the_tich = dai * rong * cao
+
+
+print(f"Diện tích đáy hình chữ nhật = {dien_tich:.{so_le}f} cm²")
+print(f"Thể tích hình khối = {the_tich:.{so_le}f} cm³")
